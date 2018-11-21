84f19716536ffbfbe54f525fc9d836dbe31ae9cc
Mateusz Pyzowski <managervcf@gmail.com>
2018-11-21 18:22:25 +0800

    Fix bug where Redux DevTools does not work on mobile again

--- src/store/index.js
+++ src/store/index.js
@@ -8,10 +8,7 @@ import {
 
 const store = createStore(
 	rootReducer,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || null
-	)
+	compose(applyMiddleware(thunk))
 );
 
 export default store;
