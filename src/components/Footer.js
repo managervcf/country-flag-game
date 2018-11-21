5f0fa9ebc16cf8ed50052edb7520d0fb0dcdb147
Mateusz Pyzowski <managervcf@gmail.com>
2018-11-21 10:55:43 +0800

    Add open in new tab functionality to footers link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Footer = () => (
 	<footer>
-		<p>by <a href='https://github.com/managervcf/'>Mateusz Pyzowski.</a></p>
+		<p>by <a
+			href='https://github.com/managervcf/'
+			target="_blank">
+			Mateusz Pyzowski.
+		</a></p>
 	</footer>
 );
 
